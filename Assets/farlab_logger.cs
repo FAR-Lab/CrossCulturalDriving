ddae0cfc16e1c47d1d9c26e3a7378ded4b54b484
David Goedicke <da.goedicke@gmail.com>
2018-12-30 17:25:55 +0100

    started Data logging mostly works

--- Assets/farlab_logger.cs
+++ Assets/farlab_logger.cs
@@ -7,15 +7,13 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System;
-using RVP;
 
-public class farlab_logger : MonoBehaviour
-{
+public class farlab_logger : MonoBehaviour {
     //Global variables
-    private GameObject player;
+    private Transform player;
 
 
-    public static char sep = '\t'; //Separator for data values.
+    public static char sep = ';'; //Separator for data values.
 
     /**The LogVariable class is a blueprint for the variable to be logged.
      * The attributes are :-
@@ -23,141 +21,146 @@ public class farlab_logger : MonoBehaviour
      * 2) name - this is the name of the variable, this will be the column header
      * for the variable values in the log file
      * 3) val - this is the value of the variable
+     * 4) updateMethod - this is the method which is called to update the value of the variable. This method must return a string.
      *
-     * To log a variable you need to do 3 things :-
-     * 1) Declare a global LogVariable object
-     * 2) Initialize this object using its constructor in the function Start
-     * 3) Set the value of this object using the SetVal method in function Update
+     * To log a variable you need to provide three things :-
+     * 1) Dataset ID
+     * 2) Variable Name
+     * 3) Update method or its value
+     * Have a look at the examples in the Awake function.
      **/
-    public class LogVariable
-
-    {
+    public class LogVariable {
         private string dat_id;
         private string name;
         private string val;
+        private Func<string> updateMethod;
         private static List<LogVariable> allVars = new List<LogVariable>();
         private static List<string> IDs = new List<string>();
 
-      
 
-        public LogVariable(string dataset_id, string header_name, string value = "-")
-        {
+
+        public LogVariable(string dataset_id, string header_name, string value = "-") {
             this.dat_id = dataset_id;
             this.name = header_name;
             this.val = value;
-            if (!IDs.Contains(dataset_id) && !dataset_id.Equals("U")) IDs.Add(dataset_id);
+            this.updateMethod = null;
+            if (!IDs.Contains(dataset_id) && !dataset_id.Equals("U"))
+                IDs.Add(dataset_id);
             allVars.Add(this);
             allVars.Sort(CompareByDatId);
 
         }
 
-        private static int CompareByDatId(LogVariable x, LogVariable y)
-        {
+        public LogVariable(string dataset_id, string header_name, Func<string> updtMethod) {
+            this.dat_id = dataset_id;
+            this.name = header_name;
+            this.updateMethod = updtMethod;
+            if (!IDs.Contains(dataset_id) && !dataset_id.Equals("U"))
+                IDs.Add(dataset_id);
+            allVars.Add(this);
+            allVars.Sort(CompareByDatId);
+
+        }
+
+        private static int CompareByDatId(LogVariable x, LogVariable y) {
             int a = x.GetDatID().CompareTo(y.GetDatID());
-            if (a == 0) a = x.GetName().CompareTo(y.GetName());
+            if (a == 0)
+                a = x.GetName().CompareTo(y.GetName());
             return a;
 
         }
 
-        public static List<string> GetHeader()
-        {
+        public static List<string> GetHeader() {
             List<string> headers = new List<string>();
             string uni = "";
             string header = "";
-            foreach (LogVariable var in allVars)
-            {
+            foreach (LogVariable var in allVars) {
 
-                if (!var.GetDatID().Equals("U"))
-                {
-                    if (header.Equals("")) header = var.GetDatID() + sep + var.GetName() + sep;
-                    else
-                    {
-                        if (header.Split(sep)[0].Equals(var.GetDatID())) header += var.GetName() + sep;
-                        else
-                        {
+                if (!var.GetDatID().Equals("U")) {
+                    if (header.Equals(""))
+                        header = var.GetDatID() + sep + var.GetName() + sep;
+                    else {
+                        if (header.Split(sep)[0].Equals(var.GetDatID()))
+                            header += var.GetName() + sep;
+                        else {
                             headers.Add(header);
                             header = var.GetDatID() + sep + var.GetName() + sep;
                         }
                     }
-                }
-                else uni += var.GetName() + sep;
+                } else
+                    uni += var.GetName() + sep;
 
             }
             headers.Add(header);
-            for (int i = 0; i < headers.Count; i++)
-            {
+            for (int i = 0; i < headers.Count; i++) {
                 headers[i] = headers[i] + uni.TrimEnd(';') + '\n';
             }
             return headers;
         }
 
-        public static List<string> GetVals()
-        {
+        public static List<string> GetVals() {
             List<string> vals = new List<string>();
             string uni = "";
             string v = "";
 
-            foreach (LogVariable var in allVars)
-            {
-                if (!var.GetDatID().Equals("U"))
-                {
-                    if (v.Equals("")) v = var.GetDatID() + sep + var.GetVal() + sep;
-                    else
-                    {
-                        if (v.Split(sep)[0].Equals(var.GetDatID())) v += var.GetVal() + sep;
-                        else
-                        {
+            foreach (LogVariable var in allVars) {
+                if (!var.GetDatID().Equals("U")) {
+                    if (v.Equals(""))
+                        v = var.GetDatID() + sep + var.GetVal() + sep;
+                    else {
+                        if (v.Split(sep)[0].Equals(var.GetDatID()))
+                            v += var.GetVal() + sep;
+                        else {
                             vals.Add(v);
-                            v = var.GetDatID() + sep + var.GetVal() + sep; ;
+                            v = var.GetDatID() + sep + var.GetVal() + sep;
+                            ;
                         }
                     }
-                }
-                else uni += var.GetVal() + sep;
+                } else
+                    uni += var.GetVal() + sep;
             }
             vals.Add(v);
-            for (int i = 0; i < vals.Count; i++)
-            {
+            for (int i = 0; i < vals.Count; i++) {
                 vals[i] = vals[i] + uni.TrimEnd(';') + '\n';
             }
             return vals;
         }
 
-        public static List<string> GetIDs()
-        {
+        public static List<string> GetIDs() {
             return IDs;
         }
 
-        public void SetVal(string value)
-        {
+        public void SetVal(string value) {
             this.val = value;
         }
 
-        public void SetName(string name)
-        {
+        public void updateVal() {
+            this.val = this.updateMethod();
+        }
+
+        public void SetName(string name) {
             this.name = name;
         }
 
-        public void SetDatID(string dataset_id)
-        {
+        public void SetDatID(string dataset_id) {
             this.dat_id = dataset_id;
         }
 
-        public string GetDatID()
-        {
+        public string GetDatID() {
             return this.dat_id;
         }
 
-        public string GetName()
-        {
+        public string GetName() {
             return this.name;
         }
 
-        public string GetVal()
-        {
+        public string GetVal() {
+            if (updateMethod != null)
+                updateVal();
             return this.val;
         }
     }
-    
+
     private Queue<string> databuffer = new Queue<string>(); //Data buffer queue
 
     private string path; //Location of the log files
@@ -176,44 +179,37 @@ public class farlab_logger : MonoBehaviour
     NetworkStream stream; //To store the stream from the device
 
 
-    //Step 1 - Declare global LogVariable objects
-    //LogVariable pos;
-    LogVariable vel;
-    LogVariable time;
-    LogVariable frame;
-    LogVariable sp;
-    LogVariable dist;
-
-    //End Step 1
-
     // Use this for initialization
-    private void Awake()
-    {
-        time = new LogVariable("U", "Time");
-        sp = new LogVariable("D2", "Speed");
-        vel = new LogVariable("D1", "Velocity");
-        dist = new LogVariable("D2", "Distance");
-        frame = new LogVariable("U", "Frame");
-    }
-    void Start()
-    {
-      
-        player = GameObject.Find("Player");
+    private void Awake() {
+        //Example of logging various different variables.
+        foreach (VehicleInputControllerNetworked vi in FindObjectsOfType<VehicleInputControllerNetworked>()) {
+            if (vi.isLocalPlayer) {
+                player = vi.transform;
+                break;
+            }
+        }
 
-        //Step 2 - Initialize LogVariable objects using their constructors
         
+        LogVariable time = new LogVariable("U", "Time", delegate () { return Time.time.ToString(); });
+        LogVariable vel = new LogVariable("D1", "Velocity", delegate () { return player != null ? player.transform.GetComponent<Rigidbody>().velocity.ToString() : " "; });
+        LogVariable sp = new LogVariable("D1", "Position", delegate () { return player != null ? player.transform.position.ToString() : " "; });
+        LogVariable dist = new LogVariable("D1", "Distance", delegate () { return player != null ? player.transform.position.magnitude.ToString() : " "; });
+        LogVariable frame = new LogVariable("U", "Frame", delegate () { return Time.frameCount.ToString(); });
+        LogVariable pos = new LogVariable("D2", "ActionState", delegate () { return SceneStateManager.Instance.ActionState.ToString(); });
+        LogVariable timeScale = new LogVariable("D2", "timeScale", delegate () { return Time.timeScale.ToString(); });
 
-        //End Step 2
+    }
+    void Start() {
 
 
-        int epoch = (int)(System.DateTime.UtcNow - new System.DateTime(1970, 1, 1)).TotalSeconds; //Epoch Time
+        int epoch = (int)( System.DateTime.UtcNow - new System.DateTime(1970, 1, 1) ).TotalSeconds; //Epoch Time
 
-        path = @"C:\Users\Public\Documents\Unity Projects\Roll a Ball\Roll a Ball\Logs\" + epoch.ToString() + "-"; //Log file path
+        path = @"D:\Logs\" + epoch.ToString() + "-"; //Log file path
 
         //path_trig = @"C:\Users\Public\Documents\Unity Projects\Roll a Ball\Roll a Ball\Logs\" + epoch.ToString() + "trig.txt"; //Log file of triggers path;
 
-        port = 23456;
-        ip = "10.132.128.202";
+        //port = 23456;
+        //ip = "10.132.128.68";
 
         InitStream(ip, port);
 
@@ -225,72 +221,68 @@ public class farlab_logger : MonoBehaviour
         isSending = true;
         send = new Thread(ContinuousDataSend);
         send.Start();
+        Debug.Log("Started Running");
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        vel.SetVal(GetVelocity().ToString());
-        time.SetVal(GetTime().ToString());
-        frame.SetVal(GetFrame().ToString());
-        sp.SetVal(GetVelocity().magnitude.ToString());
-        dist.SetVal(GetPos(player).magnitude.ToString());
+    void Update() {
+        if (player == null) {
+            foreach (VehicleInputControllerNetworked vi in FindObjectsOfType<VehicleInputControllerNetworked>()) {
+                if (vi.isLocalPlayer) {
+                    player = vi.transform;
+                    break;
+                }
+            }
+        }
+        }
+    private void LateUpdate() {
 
-        //Step 3 - Set values of LogVariable objects
-        //End Step 3
-    }
-    private void LateUpdate()
-    {
-       
 
         EnqueueData(LogVariable.GetVals());
 
     }
-    void OnApplicationQuit()
-    {
+    void OnApplicationQuit() {
         isSending = false; //Stop data sending from the independent thread
 
         /**Checking the status of data and buffer queue and sending
          * the remaining data 
          **/
-        while (!doneSending) { }
+        Debug.Log("About to shutdown");
+        int i = 0;
+        while (!doneSending) { i++; }
         CloseStream();
         CloseLogs();
+        Debug.Log(" shutdown after " + i.ToString());
 
         Debug.Log(GetFrame().ToString());
+        Debug.Log(GetTime().ToString());
+        Debug.Log(GetVelocity(player.gameObject).ToString());
+        Debug.Log(GetPos(player.gameObject).ToString());
 
     }
 
-    void EnqueueData(List<string> data)
-    { 
-        foreach (string i in data)
-        {
+    void EnqueueData(List<string> data) {
+        foreach (string i in data) {
             databuffer.Enqueue(i);
         }
     }
 
-    void InitLogs()
-    {
+    void InitLogs() {
         logs = new List<StreamWriter>();
-        for (int i = 0; i < LogVariable.GetIDs().Count; i++)
-        {
+        for (int i = 0; i < LogVariable.GetIDs().Count; i++) {
             StreamWriter f = File.AppendText(path + LogVariable.GetIDs()[i] + ".txt");
             logs.Add(f);
         }
     }
 
-    void CloseLogs()
-    {
-        foreach (StreamWriter f in logs)
-        {
+    void CloseLogs() {
+        foreach (StreamWriter f in logs) {
             f.Close();
         }
     }
 
-    void InitStream(string ip, int port)
-    {
-        try
-        {
+    void InitStream(string ip, int port) {
+        try {
             /**Create a TcpClient.
              * Note, for this client to work you need to have a TcpServer 
              * connected to the same address as specified by the ip, port
@@ -299,108 +291,82 @@ public class farlab_logger : MonoBehaviour
             client = new TcpClient(ip, port);
 
             stream = client.GetStream(); // Client stream for reading and writing.
+            Debug.Log(stream);
         }
-        catch (ArgumentNullException e)
-        {
+        catch (ArgumentNullException e) {
             Debug.Log("ArgumentNullException: " + e.ToString());
         }
-        catch (SocketException e)
-        {
+        catch (SocketException e) {
             Debug.Log("SocketException: " + e.ToString());
         }
     }
 
-    void CloseStream()
-    {
+    void CloseStream() {
         stream.Close();
         client.Close();
     }
 
-    void Flush()
-    {
-        foreach (StreamWriter f in logs)
-        {
+    void Flush() {
+        foreach (StreamWriter f in logs) {
             f.Flush();
         }
     }
 
     //Function to send the first element of the buffer queue
-    void DataSend(string data, bool network)
-    {
+    void DataSend(string data, bool network) {
         //check whether queue is empty
-        try
-        {
-            if (network)
-            {
+        try {
+            if (network) {
                 byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
                 stream.Write(msg, 0, msg.Length);
-            }
-            else if (!network)
-            {
+            } else if (!network) {
                 int id_index = LogVariable.GetIDs().IndexOf(data.Split(sep)[0]);
                 logs[id_index].Write(data);
 
             }
         }
-        catch (Exception e)
-        {
+        catch (Exception e) {
             Debug.Log(e);
             Debug.Log(data);
         }
-                   
+
     }
     //Getting the velocity of the object
-    Vector3 GetVelocity()
-    {
-        foreach (BasicNetworkInput p in FindObjectsOfType<BasicNetworkInput>())
-        {
-            if (p.isLocalPlayer)
-            {
-                return p.GetComponent<Rigidbody>().velocity;
-            }
-        }
-        return Vector3.zero;
+    Vector3 GetVelocity(GameObject obj) {
+        return obj.transform.GetComponent<Rigidbody>().velocity;
     }
 
     //Getting the position of the object
-    Vector3 GetPos(GameObject obj)
-    {
+    Vector3 GetPos(GameObject obj) {
         return obj.transform.position;
     }
 
 
-    bool GetState(GameObject obj)
-    {
+    bool GetState(GameObject obj) {
         return obj.activeSelf;
     }
 
     //Getting the elapsed time
-    float GetTime()
-    {
+    float GetTime() {
         return Time.time;
     }
 
     //Getting the frame count
-    int GetFrame()
-    {
+    int GetFrame() {
         return Time.frameCount;
     }
 
-    void ContinuousDataSend()
-    {
+    void ContinuousDataSend() {
         string dat = "";
         string dat2 = "";
         int count = 0;
-        while (isSending)
-        {
-            if (databuffer.Count != 0)
-            {
+        while (isSending) {
+            if (databuffer.Count != 0) {
                 count += 1;
                 dat = databuffer.Dequeue();
                 dat2 += dat;
                 DataSend(dat, false);
-                if (count == 20)
-                {
+                if (count == 20) {
                     DataSend(dat2, true);
                     count = 0;
                     dat2 = "";
@@ -409,18 +375,16 @@ public class farlab_logger : MonoBehaviour
             }
         }
 
-        while (databuffer.Count != 0)
-            {
-                dat = databuffer.Dequeue();
-                DataSend(dat, false);
-                dat2 += dat;
-                
-            }
+        while (databuffer.Count != 0) {
+            dat = databuffer.Dequeue();
+            DataSend(dat, false);
+            dat2 += dat;
+
+        }
         DataSend(dat2, true);
         doneSending = true;
         Debug.Log(dat);
         Debug.Log(dat2);
 
-        }
-   
+    }
 }
