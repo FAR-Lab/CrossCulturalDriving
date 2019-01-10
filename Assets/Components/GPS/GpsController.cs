7f1c19d0a91e5398af258d2f58b295c3b9826011
David Goedicke <da.goedicke@gmail.com>
2019-01-10 14:46:17 +0200

    blocking lane

---
+++ Assets/Components/GPS/GpsController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GpsController : MonoBehaviour {
+
+  public enum Direction { Straight, Left, Right};
+
+  public Sprite straightImage, leftImage, rightImage;
+
+  public Image gpsImagePlane;
+
+  public Direction defaultDirection;
+
+  // Use this for initialization
+  void Start () {
+    gpsImagePlane.sprite = spriteForDirection(defaultDirection);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+  Sprite spriteForDirection(Direction d) {
+    switch(d) {
+      case Direction.Straight:
+        return straightImage;
+      case Direction.Left:
+        return leftImage;
+      case Direction.Right:
+        return rightImage;
+      default:
+        return null;
+    }
+  }
+
+  public void SetDirection(Direction newDirection) {
+    gpsImagePlane.sprite = spriteForDirection(newDirection);
+  }
+}
---
+++ Assets/Components/GPS/GpsTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GpsTrigger : MonoBehaviour {
+
+  public GpsController.Direction setDirection;
+
+  private void OnTriggerEnter(Collider other) {
+    GpsController c = other.GetComponentInChildren<GpsController>();
+    if (c != null) {
+      c.SetDirection(setDirection);
+    }
+  }
+}
