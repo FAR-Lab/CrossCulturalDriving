38d3c11dbe47dec1ee2738074fb1183638c542b9
DavidGoedicke <2676840+DavidGoedicke@users.noreply.github.com>
2021-09-29 10:08:47 -0400

    moving  to 202.3.12

--- Assets/Scripts/BaseScripts/ConfigFileLoading.cs
+++ Assets/Scripts/BaseScripts/ConfigFileLoading.cs
@@ -72,6 +72,7 @@ public class ConfigFileLoading : MonoBehaviour
    private Dictionary<string, string> LoadDict()
     {
         Debug.Log("Trying to load from: "+m_path);
+            
     Dictionary<string, string> dict = File
         .ReadLines(@m_path)
         .Where(line => !string.IsNullOrEmpty(line)) // to be on the safe side
