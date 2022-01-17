928f5e8e449468b52111934ce476e12ba879697c
David Goedicke <2676840+DavidGoedicke@users.noreply.github.com>
2022-01-16 20:00:14 -0500

    added mirrors  but loading still crashes the client

--- Assets/Scripts/Startup/StartServerClientGUI.cs
+++ Assets/Scripts/Startup/StartServerClientGUI.cs
@@ -9,25 +9,27 @@ public class StartServerClientGUI : MonoBehaviour
     public Texture2D offIcon;
     private bool ServerStarted = false;
     private bool ClientStarted = false;
-    void OnGUI () 
-    {
+    
+    
+    void OnGUI () {
+        if (ServerStarted || ClientStarted)
+            return;
         GUI.Box(new Rect(10, 10, 120, 80), "Controlls");
         if (GUI.Button(new Rect(20, 30, 80, 20), "start server")) {
             Debug.Log("Server Started.");
             ServerStarted = true;
+            GetComponent<ConnectionAndSpawing>().Setlanguage("English");
+            GetComponent<ConnectionAndSpawing>().StartAsServer();
+            this.enabled = false;
         }
 
         if (GUI.Button(new Rect(20, 60, 80, 20), "start client")) {
             Debug.Log("Client Started.");
             ClientStarted = true;
-        }
-
-        if (ServerStarted) {
-            GUI.Box(new Rect(105, 30, 20, 20), onIcon);
-        }
-        
-        if (ClientStarted) {
-            GUI.Box(new Rect(105, 60, 20, 20), onIcon);
+            GetComponent<ConnectionAndSpawing>().SetParticipantOrder(ParticipantOrder.A);
+            GetComponent<ConnectionAndSpawing>().Setlanguage("English");
+            GetComponent<ConnectionAndSpawing>().StartAsClient();
+            this.enabled = false;
         }
     }
 
