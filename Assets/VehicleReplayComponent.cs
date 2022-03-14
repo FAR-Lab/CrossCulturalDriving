fb1e7010fab39d2528ae00da9f9f3c85c53ee55d
Hauke Sandhaus <da.goedicke@gmail.com>
2022-03-13 22:15:17 -0400

    serversideQn update plus rerunCamera automation

--- Assets/VehicleReplayComponent.cs
+++ Assets/VehicleReplayComponent.cs
@@ -1,3 +1,4 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,7 +8,7 @@ using UltimateReplay;
 public class VehicleReplayComponent : ReplayBehaviour
 {
     
-    [ReplayVar]
+ 
     public char participantOrder;
     // Start is called before the first frame update
     void Start()
