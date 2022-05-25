f38ded55d4882ec9cf57f1ae2b950ed3b4b9a41e
David Goedicke <da.goedicke@gmail.com>
2022-05-25 17:02:24 -0400

    Image showing during qn

--- Assets/Scripts/ScenarioManager.cs
+++ Assets/Scripts/ScenarioManager.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using System.Security.Cryptography.X509Certificates;
 using Newtonsoft.Json;
+using Oculus.Platform;
 using UnityEngine;
 using Application = UnityEngine.Application;
 
-public class ScenarioManager : MonoBehaviour {
-    public TextAsset  QuestionairToAsk;
+public class ScenarioManager : MonoBehaviour
+{
+    public TextAsset QuestionairToAsk;
 
     public string conditionName;
 
@@ -14,13 +17,12 @@ public class ScenarioManager : MonoBehaviour {
 
     public SceneField VisualSceneToUse;
     QNSelectionManager qnmanager;
-    public bool ready { get; private set; } // property
+    public bool ready{ get; private set; } // property
     private Dictionary<ParticipantOrder, Pose> MySpawnPositions;
 
 
-
     private Transform MyLocalClient;
-   
+
 
     public GpsController.Direction StartingDirectionParticipantA;
     public GpsController.Direction StartingDirectionParticipantB;
@@ -31,62 +33,57 @@ public class ScenarioManager : MonoBehaviour {
 
 
     public List<CameraSetupXC> CameraSetups;
-    
-    
-    void Start() {
-        qnmanager  = Instantiate(
+
+
+  
+
+    void Start(){
+        qnmanager = Instantiate(
             QuestionairPrefab).GetComponent<QNSelectionManager>();
         qnmanager.gameObject.SetActive(false);
         GetSpawnPoints();
         ready = true;
-        
+
         if (ConnectionAndSpawing.Singleton.ServerisRunning ||
-            ConnectionAndSpawing.Singleton.ServerState == ActionState.RERUN)
-        {
-           
-        }
-
+            ConnectionAndSpawing.Singleton.ServerState == ActionState.RERUN){ }
     }
 
 
-    
-    void Update() {
-     
-    }
+    void Update(){ }
 
-    public Pose? GetStartPose(ParticipantOrder val) {
+    public Pose? GetStartPose(ParticipantOrder val){
         GetSpawnPoints();
 
-        if (MySpawnPositions != null && MySpawnPositions.Count > 0 && MySpawnPositions.ContainsKey(val)) {
+        if (MySpawnPositions != null && MySpawnPositions.Count > 0 && MySpawnPositions.ContainsKey(val)){
             return MySpawnPositions[val];
         }
-        else {
+        else{
             Debug.Log("Did not find an assigned spawn point");
             return null;
         }
     }
-    
 
-    private void GetSpawnPoints() {
-        if (MySpawnPositions == null || MySpawnPositions.Count == 0) {
+
+    private void GetSpawnPoints(){
+        if (MySpawnPositions == null || MySpawnPositions.Count == 0){
             MySpawnPositions = new Dictionary<ParticipantOrder, Pose>();
-            foreach (SpawnPosition sp in GetComponentsInChildren<SpawnPosition>()) {
+            foreach (SpawnPosition sp in GetComponentsInChildren<SpawnPosition>()){
                 var transform1 = sp.transform;
                 MySpawnPositions[sp.StartingId] = new Pose(transform1.position, transform1.rotation);
             }
         }
     }
-    
 
-    public void RunQuestionairNow(Transform MyLocalClient_) {
+
+    public void RunQuestionairNow(Transform MyLocalClient_){
         MyLocalClient = MyLocalClient_;
         Transform MyCar = MyLocalClient.GetComponent<ParticipantInputCapture>().GetMyCar();
         MyLocalClient.GetComponent<ParticipantInputCapture>().NewScenario();
-       
+
         qnmanager.gameObject.SetActive(true);
         qnmanager.transform.localScale *= 0.1f;
-        qnmanager.setRelativePosition(MyCar, -0.38f, 1.14f,0.6f);
-        if (QuestionairToAsk !=null) {
+        qnmanager.setRelativePosition(MyCar, -0.38f, 1.14f, 0.6f);
+        if (QuestionairToAsk != null){
             Debug.Log("about to setup QN");
             qnmanager.startAskingTheQuestionairs(MyLocalClient, conditionName, ConnectionAndSpawing.Singleton.lang);
             Debug.Log("Started asking questions!");
@@ -95,7 +92,6 @@ public class ScenarioManager : MonoBehaviour {
         foreach (QnCaptureScreenShot screenShot in FindObjectsOfType<QnCaptureScreenShot>()){
             if (screenShot.ContainsPO(ConnectionAndSpawing.Singleton.ParticipantOrder)){
                 if (screenShot.triggered){
-
                     qnmanager.AddImage(screenShot.GetTexture());
                     MyLocalClient.GetComponent<ParticipantInputCapture>()
                         .InitiateImageTransfere(screenShot.GetTexture().EncodeToJPG(50));
@@ -105,8 +101,10 @@ public class ScenarioManager : MonoBehaviour {
         }
     }
 
-    public Dictionary<ParticipantOrder, GpsController.Direction> GetStartingPositions() {
-        return new Dictionary<ParticipantOrder, GpsController.Direction>() {
+
+
+    public Dictionary<ParticipantOrder, GpsController.Direction> GetStartingPositions(){
+        return new Dictionary<ParticipantOrder, GpsController.Direction>(){
             {ParticipantOrder.A, StartingDirectionParticipantA},
             {ParticipantOrder.B, StartingDirectionParticipantB},
             {ParticipantOrder.C, StartingDirectionParticipantC},
@@ -116,48 +114,40 @@ public class ScenarioManager : MonoBehaviour {
         };
     }
 
-    public TextAsset GetQuestionFile()
-    {
+    public TextAsset GetQuestionFile(){
         return QuestionairToAsk;
     }
-    
-   public List<QuestionnaireQuestion>   ReadString(string asset)
-    {
+
+    public List<QuestionnaireQuestion> ReadString(string asset){
         return JsonConvert.DeserializeObject<List<QuestionnaireQuestion>>(asset);
     }
 
 
-   public static string OverwriteQNDataFolderName = "QN_Updates";
-   public List<QuestionnaireQuestion> GetQuestionObject()
-   {
-       string p = Path.Combine(Application.persistentDataPath, OverwriteQNDataFolderName);
-       string file = Path.Combine(Application.persistentDataPath, OverwriteQNDataFolderName,QuestionairToAsk.name+".json");
-      Debug.Log(file);
-       if (!Directory.Exists(p))
-       {
-           Directory.CreateDirectory(p);
-       }
-       if (!File.Exists(file))
-       {
-           File.WriteAllText(file,QuestionairToAsk.text);
-           
-       }
-       
-       
-       
-       List<QuestionnaireQuestion> outval=ReadString(File.ReadAllText(file));
+    public static string OverwriteQNDataFolderName = "QN_Updates";
 
-       int startCounter = 0;
-       foreach (QuestionnaireQuestion q in outval)
-       {
-           q.setInternalID(startCounter);
-           startCounter++;
-       }
+    public List<QuestionnaireQuestion> GetQuestionObject(){
+        string p = Path.Combine(Application.persistentDataPath, OverwriteQNDataFolderName);
+        string file = Path.Combine(Application.persistentDataPath, OverwriteQNDataFolderName,
+            QuestionairToAsk.name + ".json");
+        Debug.Log(file);
+        if (!Directory.Exists(p)){
+            Directory.CreateDirectory(p);
+        }
+
+        if (!File.Exists(file)){
+            File.WriteAllText(file, QuestionairToAsk.text);
+        }
 
 
-       return outval;
-   }
+        List<QuestionnaireQuestion> outval = ReadString(File.ReadAllText(file));
 
-   
+        int startCounter = 0;
+        foreach (QuestionnaireQuestion q in outval){
+            q.setInternalID(startCounter);
+            startCounter++;
+        }
+
+
+        return outval;
+    }
 }
-
