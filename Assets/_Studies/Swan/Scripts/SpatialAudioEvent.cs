620d4f43c18dec79110379abb3d661f918edd2a5
Amelia_Fatykhova <afatykhova3@gatech.edu>
2024-04-09 11:55:22 -0400

    Added /swan/ folder in assets/_studies

---
+++ Assets/_Studies/Swan/Scripts/SpatialAudioEvent.cs
@@ -0,0 +1,6 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SpatialAudioEvent: UnityEvent<SWANEngine.AudioType, Vector3, float, float>{}
