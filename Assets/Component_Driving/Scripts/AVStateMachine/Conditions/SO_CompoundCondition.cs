38af66439b4e848c096196480e5f887e7bc5bdf2
Raymond Lin <cy546@cornell.edu>
2024-10-14 08:55:56 -0400

    Update Compound Logic

--- Assets/Component_Driving/Scripts/AVStateMachine/Conditions/SO_CompoundCondition.cs
+++ Assets/Component_Driving/Scripts/AVStateMachine/Conditions/SO_CompoundCondition.cs
@@ -1,26 +1,75 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum LogicalOperator
+{
+    And,
+    Or,
+    Nand,
+    Nor,
+    Not
+}
+
 public class SO_CompoundCondition : SO_TransitionCondition
 {
+    public LogicalOperator logicalOperator;
     public SO_TransitionCondition[] conditions;
-    public bool invertResult;
-    
-    public override bool IsConditionMet(SC_AVContext context) {
-        bool result = true;
 
-        foreach (var condition in conditions) {
-            if (!condition.IsConditionMet(context)) {
-                result = false;
-                break;
-            }
+    public override bool IsConditionMet(SC_AVContext context)
+    {
+        switch (logicalOperator)
+        {
+            case LogicalOperator.And:
+                foreach (var condition in conditions)
+                {
+                    if (!condition.IsConditionMet(context))
+                    {
+                        return false;
+                    }
+                }
+                return true;
+
+            case LogicalOperator.Or:
+                foreach (var condition in conditions)
+                {
+                    if (condition.IsConditionMet(context))
+                    {
+                        return true;
+                    }
+                }
+                return false;
+
+            case LogicalOperator.Nand:
+                foreach (var condition in conditions)
+                {
+                    if (!condition.IsConditionMet(context))
+                    {
+                        return true;
+                    }
+                }
+                return false;
+
+            case LogicalOperator.Nor:
+                foreach (var condition in conditions)
+                {
+                    if (condition.IsConditionMet(context))
+                    {
+                        return false;
+                    }
+                }
+                return true;
+
+            case LogicalOperator.Not:
+                if (conditions.Length != 1)
+                {
+                    throw new InvalidOperationException("NOT operator requires exactly one condition.");
+                }
+                return !conditions[0].IsConditionMet(context);
+
+            default:
+                throw new NotSupportedException("Unsupported logical operator.");
         }
-
-        if (invertResult) {
-            result = !result;
-        }
-
-        return result;
     }
 }
