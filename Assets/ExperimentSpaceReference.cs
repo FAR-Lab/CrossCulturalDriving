f0596f895317f1a14b7184266ef7a07b3a75873d
XC <crossculturedriving@gmail.com>
2025-08-06 19:56:55 +0300

    Update ExperimentSpaceReference.cs

--- Assets/ExperimentSpaceReference.cs
+++ Assets/ExperimentSpaceReference.cs
@@ -203,27 +203,28 @@ public class ExperimentSpaceReference : MonoBehaviour {
             WorkingArea.Add(borderPoint);
         }
 
-        LineRenderer lineRenderer = null;
+// #if UNITY_EDITOR
+//         LineRenderer lineRenderer = null;
         
-        // if there are no linerenderers in this gameobject, create them
-        if (transform.GetComponent<LineRenderer>() == null) {
-            lineRenderer = gameObject.AddComponent<LineRenderer>();
-            lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
-            lineRenderer.startColor = Color.red;
-            lineRenderer.endColor = Color.red;
-            lineRenderer.startWidth = 0.1f;
-            lineRenderer.endWidth = 0.1f;
-            lineRenderer.positionCount = WorkingArea.Count + 1;
-        }
+//         // if there are no linerenderers in this gameobject, create them
+//         if (transform.GetComponent<LineRenderer>() == null) {
+//             lineRenderer = gameObject.AddComponent<LineRenderer>();
+//             lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
+//             lineRenderer.startColor = Color.red;
+//             lineRenderer.endColor = Color.red;
+//             lineRenderer.startWidth = 0.1f;
+//             lineRenderer.endWidth = 0.1f;
+//             lineRenderer.positionCount = WorkingArea.Count + 1;
+//         }
 
-        lineRenderer = GetComponent<LineRenderer>();
+//         lineRenderer = GetComponent<LineRenderer>();
         
-        for (var i = 0; i < WorkingArea.Count; i++) {
-            lineRenderer.SetPosition(i, WorkingArea[i].position);
-        }
+//         for (var i = 0; i < WorkingArea.Count; i++) {
+//             lineRenderer.SetPosition(i, WorkingArea[i].position);
+//         }
 
-        lineRenderer.SetPosition(WorkingArea.Count, WorkingArea[0].position);
-       
+//         lineRenderer.SetPosition(WorkingArea.Count, WorkingArea[0].position);
+// #endif
         
     }
 
